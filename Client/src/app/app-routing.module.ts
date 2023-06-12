7ff54d13cc3a3fa8ab845b36f6f2133bdfbb81b4
Vaishnavi0808 <sarodevaishnavi3@gmail.com>
2023-06-12 23:32:56 +0530

    ui added

---
+++ Client/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent,
+    data:{
+      toolbarShadowEnabled: false
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
